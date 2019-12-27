f08dc911d395dd44e31f6ebd8f2bebb09cd59d5d
John Reiner <johnreiner1990@gmail.com>
2019-12-27 14:35:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,12 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	{:railroads => {:pieces => 4}}
+	{:railroads => {
+		:pieces => 4,
+		:rent_in_dollars => 200
+
+		}
+	}
 
 end
 
