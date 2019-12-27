521b7ec10d8ea48f3bb436961c60e036dbd03f4e
John Reiner <johnreiner1990@gmail.com>
2019-12-27 14:44:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -19,8 +19,8 @@ def monopoly_with_third_tier
 		:names => {
 			:reading_railroad => {},
 			:pennsylvania_railroad => {},
-			:b_and_o_railroad => {},
-			:short_line_railroad => {},
+			:bando_railroad => {},
+			:shortline_railroad => {},
 		},
 
 		}
