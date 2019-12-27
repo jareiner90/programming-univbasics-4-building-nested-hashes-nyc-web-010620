2edffb1a1974b221c81897d28aa869f1222b0529
John Reiner <johnreiner1990@gmail.com>
2019-12-27 14:39:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,12 @@ def monopoly_with_third_tier
 			:key3 => 1,
 			:key4 => 1,
 		},
-		:names => 'names',
+		:names => {
+			:key1 => 1,
+			:key2 => 1,
+			:key3 => 1,
+			:key4 => 1,
+		},
 
 		}
 	}
